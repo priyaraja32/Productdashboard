2cdbee6db4de837fd81e9a298319f9569d541517
priyaraja32 <rpriyanka31032004@gmail.com>
2025-12-23 20:45:38 +0530

    change footer and header file

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,3 @@
-// const Footer = () => {
-//   return (
-//     <footer className="bg-gray-800 text-white text-center py-4 mt-auto">
-//       @ 2025 Product Dashboard. All Rights Reserved.
-//     </footer>
-//   );
-// };
-
-// export default Footer;
 
 const Footer = () => {
   return (
